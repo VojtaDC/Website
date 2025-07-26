319480afc5889b14fb22963f810e450b1e82e14b
Vojta De Coninck <vojtadeconinck@gmail.com>
2025-07-26 11:45:46 +0200

    Add GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://JouwGitHubUsername.github.io',
+  base: '/repository-naam',
+  output: 'static',
 });
